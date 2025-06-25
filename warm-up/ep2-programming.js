c1ccd036f68be0abafff3da4f273bc80407b1232
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-06-25 12:13:08 +0530

    Overview of basics of javascript datatypes

---
+++ warm-up/ep2-programming.js
@@ -0,0 +1,12 @@
+let firstname = 100;
+let lastname = 200;
+let fullname = firstname + " " + lastname;
+console.log(fullname);
+// 1. The space causes both numbers to be converted to strings and concatenated
+
+let a = 10;
+let b = "keerthivasan";
+let c = a + b;
+// 2. Type coercion occurs so, the number 10 is converted to a string and concatenated with "keerthivasan".
+
+// 3. Wherever JavaScript code runs, there must be a JavaScript engine present. Without it, the code cannot be executed.
