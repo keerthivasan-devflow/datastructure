4057ddb8f14f36500391165441b5239350176b29
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-06-25 12:23:37 +0530

    Nested for-loop execution

---
+++ warm-up/ep7-loop-in-loop.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 5; i++) {
+  for (let j = 0; j < i; j++) {
+    console.log("i = " + i, "j = " + j);
+  }
+}
+
+for (let i = 5; i > 0; i--) {
+  for (let j = 0; j < i; j++) {
+    console.log("i = " + i, "j = " + j);
+  }
+}
