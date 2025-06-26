98698f2e82cf5421f9ea430f9804a8acc38e6e6b
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-06-26 10:58:07 +0530

    Exercise - move zeroes to the end of an array

---
+++ array/moveZeroes.js
@@ -0,0 +1,14 @@
+function moveZeroes(nums) {
+  let pos = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != 0) {
+      nums[pos] = nums[i];
+      pos = pos + 1;
+    }
+  }
+  for (let i = pos; i < nums.length; i++) nums[i] = 0;
+  return nums;
+}
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0, 0, 2, 4]));
