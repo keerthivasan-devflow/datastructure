9fc0b9f384e9d43c508872fbec6076148914b4a9
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-06-26 10:59:04 +0530

    Exercise - Find the maximum consecutive 1's in the given array

---
+++ array/maxConsecutiveOnes.js
@@ -0,0 +1,15 @@
+function maxConsecutiveOnes(nums) {
+  let currCount = 0;
+  let maxCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) currCount++;
+    else {
+      maxCount = Math.max(currCount, maxCount);
+      currCount = 0;
+    }
+  }
+  return Math.max(currCount, maxCount);
+}
+
+console.log(maxConsecutiveOnes([1, 1, 0, 0, 1, 1, 1, 1, 1]));
+console.log(maxConsecutiveOnes([1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0]));
