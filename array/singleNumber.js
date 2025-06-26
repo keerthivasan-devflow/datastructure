8c8b3cb46d4132ec53b0ae3f00d17984abfd63f3
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-06-26 11:01:00 +0530

    Exercise - Find the single occurence of a number in the given array

---
+++ array/singleNumber.js
@@ -0,0 +1,32 @@
+// Brute-force approach
+// Time Complexity - O(n)
+// Space Complexity - O(n/2) - O(n)
+
+function singleNumber(nums) {
+  const hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1;
+    } else {
+      hash[nums[i]]++;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]] === 1) return nums[i];
+  }
+}
+
+// Optimized Code
+
+function findSingleNumber(nums) {
+  let xor = 0;
+  for (let i = 0; i < nums.length; i++) {
+    xor = xor ^ nums[i];
+  }
+  return xor;
+}
+
+console.log(singleNumber([2, 1, 2, 3, 3]));
+console.log(findSingleNumber([1, 3, 4, 2, 3, 2, 1]));
