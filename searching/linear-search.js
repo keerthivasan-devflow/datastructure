e2aff07365907b0363b37f8d707f8a54d2fa8f00
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-06-27 15:51:53 +0530

    Linear search implementation using advanced for..of loop in javascript(Es6 features)

--- searching/linear-search.js
+++ searching/linear-search.js
@@ -2,8 +2,18 @@ function linearSearch(nums, target) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] == target) return i;
   }
-  return "Element does not exist! " + Number(-1);
+  return -1;
 }
-
 console.log(linearSearch([3, 4, -19, 73, 0], 73));
 console.log(linearSearch([3, 4, -19, 73, 0], 50));
+
+// Linear Search Algorithm - Using built-in functions and for..of loop
+function built_In_Linear_Search(arr, target) {
+  for (let val of arr) {
+    if (val === target) return arr.indexOf(val);
+  }
+  return -1;
+}
+console.log(built_In_Linear_Search([12, 45, 22, 0, 89], 58));
+console.log(built_In_Linear_Search([12, 45, 22, 0, 89], 22));
+console.log(built_In_Linear_Search([12, 45, 22, 0, 89], 89));
