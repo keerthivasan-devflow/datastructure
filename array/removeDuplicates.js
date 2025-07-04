a6b5768893913d3af736de0bf4af4d2290a6a6c1
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-07-04 21:36:31 +0530

    Updated remove duplicates code in array concept

--- array/removeDuplicates.js
+++ array/removeDuplicates.js
@@ -1,5 +1,5 @@
 // Write a function that has an integer array[nums] sorted in non-decreasing order, remove the
-// duplicates in-place such that each unique element appear only once. The relative order of 
+// duplicates in-place such that each unique element appear only once. The relative order of
 // the elements should be kept the same. Then return the number of unique elements in [nums]
 
 function removeDuplicates(nums) {
