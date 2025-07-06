8eb378aacf7fb7b5111ff81fb10aac8d6dd98e78
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-07-06 13:25:30 +0530

    Singly-linked-list implementation

--- linked-lists/singly-linked-list.js
+++ linked-lists/singly-linked-list.js
@@ -77,6 +77,40 @@ MyLinkedList.prototype.middleOfLinkedList = function (head) {
   return slow;
 };
 
+// Linked List Cycle Detection
+// Approach 1 - using Hash Table
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+var hasCycle = function (head) {
+  let seenNodes = new Set();
+  let curr = head;
+  while (curr) {
+    if (seenNodes.has(curr)) {
+      return true;
+    }
+    seenNodes.add(curr);
+    curr = curr.next;
+  }
+  return false;
+};
+
+// Approach 2 - Floyd's Cycle Algorithm
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+var hasCycle = function (head) {
+  if (!head) return false;
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+};
+
 let list = new MyLinkedList();
 list.addAtHead(10);
 list.addAtTail(20);
