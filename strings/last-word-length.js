b265824cdc110e0a87f9fd2c0bfc84d7ca1642d0
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-07-06 13:29:21 +0530

    Optimized the find the last word length code implementation

--- strings/last-word-length.js
+++ strings/last-word-length.js
@@ -1,9 +1,40 @@
+// Approach 1 - Using built-in functions
 function LastWordLengthTraditional(str) {
   str = str.trim();
   str = str.split(" ");
-  return str[str.length - 1].length;
+  return str.at(-1).length;
+}
+console.log(LastWordLengthTraditional("    I am   a        good boy       "));
+console.log(LastWordLengthTraditional("Javascript is a scripting language"));
+console.log(LastWordLengthTraditional(" "));
+console.log(LastWordLengthTraditional(""));
+
+// Approach 2 - opitmized code using double-while loop
+function LastWordLength(str) {
+  let n = str.length - 1;
+  let count = 0;
+
+  while (n >= 0) {
+    if (str[n] === " ") {
+      n--;
+    } else {
+      break;
+    }
+  }
+
+  while (n >= 0) {
+    if (str[n] !== " ") {
+      count++;
+      n--;
+    } else {
+      break;
+    }
+  }
+
+  return count;
 }
 
+// Approach 3 - opitmized code
 function LastWordLength(str) {
   let n = str.length - 1;
   let count = 0;
@@ -20,11 +51,12 @@ function LastWordLength(str) {
   return count;
 }
 
-// console.log(LastWordLength("    I am   a        good boy       "));
-// console.log(LastWordLength("Javascript is a scripting language"));
-// console.log(LastWordLength(" "));
-// console.log(LastWordLength(""));
+console.log(LastWordLength("    I am   a        good boy       "));
+console.log(LastWordLength("Javascript is a scripting language"));
+console.log(LastWordLength(" "));
+console.log(LastWordLength(""));
 
+// Approach 4 - opitmized code using single-while loop
 function LastWordLengthOptimized(str) {
   let n = str.length - 1;
   let count = 0;
