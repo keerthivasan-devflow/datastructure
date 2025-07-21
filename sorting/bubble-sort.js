d131a82e337fae87ad54ebabfa701c67ab5bb137
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-07-21 13:31:17 +0530

    Bubble sort code has been updated

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,12 +1,10 @@
 function bubblesort(nums) {
   let n = nums.length;
-  let temp = 0;
-  let swapped;
   for (let i = 0; i < n - 1; i++) {
-    swapped = false;
+    let swapped = false;
     for (let j = 0; j < n - 1 - i; j++) {
       if (nums[j] > nums[j + 1]) {
-        temp = nums[j];
+        let temp = nums[j];
         nums[j] = nums[j + 1];
         nums[j + 1] = temp;
         swapped = true;
@@ -19,3 +17,5 @@ function bubblesort(nums) {
 
 console.log(bubblesort([5, 2, 4, 1])); // Random array integers to sort in ascending order
 console.log(bubblesort([10, 20, 30, 40, 50])); // To check for an already sorted array
+
+// But once a swap happens, swapped = true forever – so the loop never breaks early, even when the array is sorted mid-way.
