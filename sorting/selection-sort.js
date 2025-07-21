ed82a416123614fedbed581ceaa4dbdba7caed06
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-07-21 13:43:03 +0530

    selection sort code has been updated

--- sorting/selection-sort.js
+++ sorting/selection-sort.js
@@ -1,7 +1,8 @@
 function selectionsort(nums) {
-  for (let i = 0; i < nums.length; i++) {
+  let n = nums.length;
+  for (let i = 0; i < n - 1; i++) {
     let min = i;
-    for (let j = i + 1; j < nums.length; j++) {
+    for (let j = i + 1; j < n; j++) {
       if (nums[j] < nums[min]) {
         min = j;
       }
