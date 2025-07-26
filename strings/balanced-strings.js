815191ca4239392324c5bd13f974d0a055ca5229
Keerthivasan Mani <keerthivasan.mani1604@outlook.com>
2025-07-26 23:02:14 +0530

    Adjusted the whitespaces for the clean code

--- strings/balanced-strings.js
+++ strings/balanced-strings.js
@@ -2,19 +2,22 @@
 function BalancedStrings(s) {
   let count = 0;
   let temp = 0;
+
   for (let i = 0; i < s.length; i++) {
     if (s[i] == s[0]) temp += 1;
     else temp -= 1;
     if (temp === 0) count += 1;
   }
+  
   return count;
 }
 
 // Approach 2
 function balanceString(s) {
   let count = 0;
-  let firstcount = 0,
-    secondcount = 0;
+  let firstcount = 0;
+  let secondcount = 0;
+
   for (let i = 0; i < s.length; i++) {
     if (s[i] === s[0]) firstcount++;
     else secondcount++;
@@ -25,6 +28,7 @@ function balanceString(s) {
       secondcount = 0;
     }
   }
+
   return count;
 }
 
